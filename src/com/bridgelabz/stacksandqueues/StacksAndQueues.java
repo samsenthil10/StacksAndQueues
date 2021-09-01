347532f51d872432d9391689eb85e531359cf327
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-01 06:07:21 +0530

    Added: To demonstrate a simple stack

--- src/com/bridgelabz/stacksandqueues/StacksAndQueues.java
+++ src/com/bridgelabz/stacksandqueues/StacksAndQueues.java
@@ -3,6 +3,25 @@ package com.bridgelabz.stacksandqueues;
 public class StacksAndQueues {
 
 	public static void main(String[] args) {
-
+		
+		
+		Node<Integer> item1 = new Node<>();
+		Node<Integer> item2 = new Node<>();
+		Node<Integer> item3 = new Node<>();
+		item1.setKey(70);
+		item2.setKey(30);
+		item3.setKey(56);
+		Stack<Integer> myStack= new Stack<>();
+		myStack.push(item1);
+		myStack.printMyStack();
+		System.out.println(myStack.peak());
+		System.out.println();
+		myStack.push(item2);
+		myStack.printMyStack();
+		System.out.println(myStack.peak());
+		System.out.println();
+		myStack.push(item3);
+		myStack.printMyStack();
+		System.out.println(myStack.peak());	
 	}
 }
