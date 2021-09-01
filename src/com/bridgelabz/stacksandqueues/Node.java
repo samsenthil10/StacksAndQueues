ccdf8290f23bbb4dfca1ef68048e98630049d559
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-01 06:06:01 +0530

    Added: To create a Node

--- src/com/bridgelabz/stacksandqueues/Node.java
+++ src/com/bridgelabz/stacksandqueues/Node.java
@@ -1,5 +1,20 @@
 package com.bridgelabz.stacksandqueues;
 
-public class Node {
+public class Node<K> {
 
+	private K key;
+	private Node<K> next;
+
+	public K getKey() {
+		return key;
+	}
+	public void setKey(K key) {
+		this.key = key;
+	}
+	public Node<K> getNext() {
+		return next;
+	}
+	public void setNext(Node<K> next) {
+		this.next = next;
+	}
 }
