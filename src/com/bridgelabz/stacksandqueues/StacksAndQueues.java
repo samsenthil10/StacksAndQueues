6e83788e8e4f17c3c29f64c0c58aa96ccbf57299
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-02 02:19:02 +0530

    Refactored: To demonstrate how dequeue operation works

--- src/com/bridgelabz/stacksandqueues/StacksAndQueues.java
+++ src/com/bridgelabz/stacksandqueues/StacksAndQueues.java
@@ -5,9 +5,9 @@ public class StacksAndQueues {
 	public static void main(String[] args) {
 		
 		
-		Node<Integer> item1 = new Node<>();
-		Node<Integer> item2 = new Node<>();
-		Node<Integer> item3 = new Node<>();
+		NodeImpl<Integer> item1 = new NodeImpl<>();
+		NodeImpl<Integer> item2 = new NodeImpl<>();
+		NodeImpl<Integer> item3 = new NodeImpl<>();
 		item1.setKey(56);
 		item2.setKey(30);
 		item3.setKey(70);
