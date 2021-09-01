3df100f9b04371bafe5a14b612ac8eee200b9326
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-01 06:19:14 +0530

    Added: To demonstrate how enqueue method works

--- src/com/bridgelabz/stacksandqueues/StacksAndQueues.java
+++ src/com/bridgelabz/stacksandqueues/StacksAndQueues.java
@@ -8,22 +8,18 @@ public class StacksAndQueues {
 		Node<Integer> item1 = new Node<>();
 		Node<Integer> item2 = new Node<>();
 		Node<Integer> item3 = new Node<>();
-		item1.setKey(70);
+		item1.setKey(56);
 		item2.setKey(30);
-		item3.setKey(56);
-		Stack<Integer> myStack= new Stack<>();
-		myStack.push(item1);
-		myStack.printMyStack();
-		System.out.println(myStack.peak());
+		item3.setKey(70);
+		Queue<Integer> myQueue= new Queue<>();
+		myQueue.enqueue(item1);
+		myQueue.printMyQueue();
 		System.out.println();
-		myStack.push(item2);
-		myStack.printMyStack();
-		System.out.println(myStack.peak());
+		myQueue.enqueue(item2);
+		myQueue.printMyQueue();
 		System.out.println();
-		myStack.push(item3);
-		myStack.printMyStack();
-		System.out.println(myStack.peak());	
+		myQueue.enqueue(item3);
+		myQueue.printMyQueue();
 		System.out.println();
-		myStack.peakAndPopTillEmpty();
 	}
 }
